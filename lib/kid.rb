c471d1929eec478030fbcddd890a49386c561d67
David Stewart <davidstewart@gmail.com>
2019-04-13 19:42:10 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,3 +1,4 @@
+
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
@@ -5,9 +6,8 @@ class Kid
   include Dance
   extend MetaDancing
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
-  
 end
